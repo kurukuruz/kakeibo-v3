b88b8269b647a00bce0f712ac5d1dc0e0bb7f94d
kurukuruz <kurukuruz.4dev@gmail.com>
2022-06-12 23:27:35 +0900

    日単位のダイアログを作成

---
+++ src/components/calendar/TheOneDayDialog.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import Button from 'primevue/button';
+import Dialog from 'primevue/dialog';
+import { ref } from 'vue';
+
+interface IProps {
+  date: string;
+}
+
+const props = defineProps<IProps>();
+
+const display = ref(false);
+
+const onShow = () => {
+  console.log(`show with: ${props.date}`);
+};
+</script>
+
+<template>
+  <Button
+    icon="pi pi-list"
+    @click="display = true"
+  />
+  <Dialog
+    v-model:visible="display"
+    :modal="true"
+    :header="props.date"
+    @show="onShow"
+  >
+    Content
+  </Dialog>
+</template>
