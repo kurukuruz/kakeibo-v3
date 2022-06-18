314c08a93f4195f1a81056386af4b4f73ca0f8b9
kurukuruz <kurukuruz.4dev@gmail.com>
2022-06-19 02:49:33 +0900

    カテゴリのソート順を設定

--- src/dba/categories.ts
+++ src/dba/categories.ts
@@ -1,11 +1,12 @@
-import { collection, getDocs, query } from 'firebase/firestore';
+import { collection, getDocs, orderBy, query } from 'firebase/firestore';
 import { ICategory, ICategoryDoc } from '../domains/category';
 import db from '../plugins/use-firestore';
 
 const getCategoriesCollection = (bookId: string) => collection(db, 'books', bookId, 'categories');
 
 export const getCategories = async (bookId: string):Promise<ICategoryDoc[]> => {
-  const querySnapshot = await getDocs(query(getCategoriesCollection(bookId)));
+  const q = query(getCategoriesCollection(bookId), orderBy('order'));
+  const querySnapshot = await getDocs(q);
   const categories: ICategoryDoc[] = [];
   querySnapshot.forEach((d) => {
     categories.push({
