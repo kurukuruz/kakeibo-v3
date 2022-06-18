48a11fb5153b27e34112d360d442a2cb551886f0
kurukuruz <kurukuruz.4dev@gmail.com>
2022-06-19 03:11:08 +0900

    未認証時にホーム画面に遷移するように修正

--- src/plugins/use-router.ts
+++ src/plugins/use-router.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
+import { getAuth } from 'firebase/auth';
 import TheHomePage from '../pages/TheHomePage.vue';
 import TheMainPage from '../pages/TheMainPage.vue';
 
@@ -12,6 +13,20 @@ const router = createRouter({
   //    We are using the hash history for simplicity here.
   history: createWebHashHistory(),
   routes, // short for `routes: routes`
+});
+
+router.beforeResolve((to, _, next) => {
+  if (to.path === '/') {
+    next();
+  } else {
+    getAuth().onAuthStateChanged(user => {
+      if (user) {
+        next();
+      } else {
+        next({ path: '/' });
+      }
+    });
+  }
 });
 
 export default router;
