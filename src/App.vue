7074e34a16cf0ec1b7b0c7bebac2195c244d8753
kurukuruz <kurukuruz.4dev@gmail.com>
2022-06-19 10:03:18 +0900

    フォント設定を記述

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
   <router-view />
 </template>
+
+<style>
+#app {
+  font-family: var(--font-family);
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
