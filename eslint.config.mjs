ffda1eebac1e9c145d1e3c804e1562bb8d445c80
kurukuruz <kurukuruz@local>
2024-07-26 22:54:16 +0900

    chore: eslint/stylisticルールに合うように設定ファイルの記述を修正

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,30 @@
-import pluginVue from 'eslint-plugin-vue'
+import pluginVue from 'eslint-plugin-vue';
 import eslint from '@eslint/js';
 import tsEslint from 'typescript-eslint';
-import stylistic from '@stylistic/eslint-plugin'
+import stylistic from '@stylistic/eslint-plugin';
 
 export default tsEslint.config(
   eslint.configs.recommended,
   ...tsEslint.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
   stylistic.configs.customize({
-    "arrowParens": true,
-    "braceStyle": "1tbs",
-    "semi": true
+    arrowParens: true,
+    braceStyle: '1tbs',
+    semi: true,
   }),
   {
     files: ['*.vue', '**/*.vue'],
     languageOptions: {
       parserOptions: {
-        parser: '@typescript-eslint/parser'
-      }
+        parser: '@typescript-eslint/parser',
+      },
     },
     rules: {
-      "vue/max-attributes-per-line": ["warn", {
-        "singleline": {
-        "max": 3
-        }
-      }]
-    }
-  }
+      'vue/max-attributes-per-line': ['warn', {
+        singleline: {
+          max: 3,
+        },
+      }],
+    },
+  },
 );
