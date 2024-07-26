928c15603f6d62291a4d487bb86a5eb5e02b9209
kurukuruz <kurukuruz@local>
2024-07-26 22:36:38 +0900

    chore: stylistic追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,17 @@
 import pluginVue from 'eslint-plugin-vue'
 import eslint from '@eslint/js';
 import tsEslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin'
 
 export default tsEslint.config(
   eslint.configs.recommended,
   ...tsEslint.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
+  stylistic.configs.customize({
+    "arrowParens": true,
+    "braceStyle": "1tbs",
+    "semi": true
+  }),
   {
     files: ['*.vue', '**/*.vue'],
     languageOptions: {
